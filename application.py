b1ce94cb0e2e4fd24eab276ee94e0b6b9679ef20
Kunal Modi <kunalmodi7007@gmail.com>
2022-10-10 09:55:16 +0530

    Small change

--- application.py
+++ application.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-	return 'Love from Kunal'
+	return 'Love from Kunal Modi'
 
 # main driver function
 if __name__ == '__main__':
